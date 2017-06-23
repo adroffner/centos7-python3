f8420025a53a3987856a86511778cb3a3b1e389e
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2017-06-23 18:07:23 -0400

    [EE-198] Must install the Python3 dbm library for Celery 4.0

--- run_docker.sh
+++ run_docker.sh
@@ -6,10 +6,11 @@
 # This is NOT run in "detached" (-d) mode.
 # =============================================================================
 
-REPOSITORY="att-gcs"
+REGISTRY="zlp11313.vci.att.com:5100"
+NAMESPACE="com.att.dev.argos"
 IMAGE_NAME="centos7-python3"
 TAG="3.5.2"
 
-IMAGE_TAG="${REPOSITORY}/${IMAGE_NAME}:${TAG}"
+IMAGE_TAG="${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${TAG}"
 
 docker run -ti $IMAGE_TAG
